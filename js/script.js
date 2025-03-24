16a7fcd03b7a7cf610b5b95001e47043f4a03228
Camiiino <subieheswani@gmail.com>
2025-03-24 17:24:35 +0100

    fixed menu scroll lock

--- js/script.js
+++ js/script.js
@@ -10,6 +10,46 @@ document.addEventListener('commonSectionsLoaded', function () {
     const burger = document.getElementById("burger");
     const menu = document.getElementById("menu");
 
+    (function() {
+        const keys = {37: 1, 38: 1, 39: 1, 40: 1};
+      
+        function preventDefault(e) {
+          if (e.preventDefault) e.preventDefault();
+          e.returnValue = false;
+        }
+        
+        function preventDefaultForScrollKeys(e) {
+          if (keys[e.keyCode]) {
+            preventDefault(e);
+            return false;
+          }
+        }
+        
+        let supportsPassive = false;
+        try {
+          window.addEventListener("test", null, Object.defineProperty({}, "passive", {
+            get: function () { supportsPassive = true; }
+          }));
+        } catch (e) {}
+        
+        const wheelOpt = supportsPassive ? { passive: false } : false;
+        const wheelEvent = "onwheel" in document.createElement("div") ? "wheel" : "mousewheel";
+        
+        window.disableScroll = function() {
+          window.addEventListener("DOMMouseScroll", preventDefault, false); // older Firefox
+          window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+          window.addEventListener("touchmove", preventDefault, wheelOpt); // mobile
+          window.addEventListener("keydown", preventDefaultForScrollKeys, false);
+        };
+        
+        window.enableScroll = function() {
+          window.removeEventListener("DOMMouseScroll", preventDefault, false);
+          window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+          window.removeEventListener("touchmove", preventDefault, wheelOpt);
+          window.removeEventListener("keydown", preventDefaultForScrollKeys, false);
+        };
+      })();
+
     burger.addEventListener("change", () => {
         if (menu.classList.contains("open")) {
             menu.classList.remove("open");
