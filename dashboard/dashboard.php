83b77f114ed756b2de4f0381b2f1cc24c164ea03
Camiiino <subieheswani@gmail.com>
2025-04-13 21:42:12 +0200

    updated minor routing conflict

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -1,9 +1,5 @@
 <?php
-session_start();
-if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
-    header('Location: /login.php');
-    exit;
-}
+  require_once 'auth_check.php';
 ?>
 
 <!DOCTYPE html>
